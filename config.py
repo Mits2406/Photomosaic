113d5882950fec0997881bd7d5d39e0958fe9839
Mits2406 <59138942+Mits2406@users.noreply.github.com>
2024-12-04 11:43:33 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-token = ''
-key = ''
-ya_token = ''
-#https://oauth.vk.com/authorize?client_id=7823527&group_ids=public207554117&display=page&redirect_uri=http://example.com/callback&scope=messages,photos&response_type=token&v=5.131
-#https://oauth.vk.com/authorize?client_id=7823527&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.131&state=123456
-#https://oauth.vk.com/authorize?client_id=7823527&display=page&scope=friends,groups,photos&response_type=token&v=5.92&state=123456
